528bfb82384e2c779712d0344c7692d16dc21e58
Wouter Danes <wouter.danes@gmail.com>
2024-01-10 21:23:17 +0100

    Initial setup

---
+++ lua/wouter/packer.lua
@@ -0,0 +1,45 @@
+--  This file can be loaded by calling `lua require('plugins')` from your init.vim
+--
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  require("rose-pine").setup()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+
+  use('neovim/nvim-lspconfig')
+  use('williamboman/mason.nvim')
+  use('williamboman/mason-lspconfig.nvim')
+  use('hrsh7th/nvim-cmp')
+  use('hrsh7th/cmp-nvim-lsp')
+  use('hrsh7th/cmp-buffer')
+  use('hrsh7th/cmp-path')
+  use('saadparwaiz1/cmp_luasnip')
+  use('hrsh7th/cmp-nvim-lua')
+  use('L3MON4D3/LuaSnip')
+  use('rafamadriz/friendly-snippets')
+
+  use('VonHeikemen/lsp-zero.nvim')
+end)
+

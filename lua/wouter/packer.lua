1f414fc7155722d42f8f5685504677f80d65c901
Wouter Danes <wouter.danes@postnl.nl>
2024-02-22 16:37:24 +0100

    Added copilot plugin

--- lua/wouter/packer.lua
+++ lua/wouter/packer.lua
@@ -51,6 +51,7 @@ return require('packer').startup(function(use)
 
   use('VonHeikemen/lsp-zero.nvim')
   use('lewis6991/gitsigns.nvim')
+  use("github/copilot.vim")
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
